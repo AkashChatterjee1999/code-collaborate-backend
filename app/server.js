7992dac4d3ece7ad2f14efde37175d9d1b4f51d5
akashchatterjee1999 <akashchatterjee1000@gmail.com>
2021-08-15 21:52:53 +0530

    Added numerous events and changed mutual handshaking process

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const ws = require("ws");
 const uuid = require("uuidv4");
-const server = new ws.Server({ host: "localhost", port: "3000" });
+const server = new ws.Server({ host: "localhost", port: "5050" });
 let clientMap = new Map();
 let clientInfo = new Map();
 
@@ -9,7 +9,7 @@ functionalMap = {
         let connectedClients = [];
         clientInfo.forEach((client, clientId) => {
             connectedClients.push({
-                clientId, name: client.name, profilePic: client.profilePic
+                clientId, name: client.name, profilePic: client.profilePic, location: client.clientLocation, email: client.clientEmail
             })
         });
         return {"responseEvent": "OPEN", "responseType": "acknowledge", "metadataData": { id, connectedClients }}
@@ -17,8 +17,11 @@ functionalMap = {
     onClientDisconnected: clientID => {
         return {"responseEvent": "CLIENT_DISCONNECTED", "responseType": "info", "data": { clientID }}
     },
-    onClientJoined: (clientID, clientName, profilePic) => {
-        return {"responseEvent": "CLIENT_CONNECTED", "responseType": "info", "data": { clientID, clientName, profilePic }}
+    onClientJoined: (clientID, clientName, profilePic, location, email) => {
+        return {"responseEvent": "CLIENT_CONNECTED", "responseType": "info", "data": { clientID, clientName, profilePic, location, email }}
+    },
+    acknowledgeClientInfo: (id) => {
+        return {"responseEvent": "ACKNOWLEDGE_CLIENT_INFO", "responseType": "acknowledge", "data": { id }}
     }
 }
 
@@ -26,6 +29,22 @@ initializeClient = ws => {
     let clientId = uuid.uuid();
     clientMap.set(clientId, ws);
     return functionalMap.onOpenAcknowledgement(clientId);
+}
+
+checkEmailExistedThenReturnId = (email) => {
+    console.log("This Email to be checked: ", email);
+    if(email === null || email=== undefined || !email) return undefined;
+    let keys = Array.from(clientInfo.keys());
+    for(let i=0; i<keys.length; i++) {
+        let value = clientInfo.get(keys[i]), key = keys[i];
+        console.log("lol", value.clientEmail.toString().trim().toLowerCase(), "lol1:", email.toString().trim().toLowerCase());
+        console.log(value.clientEmail.toString().trim().toLowerCase() === email.toString().trim().toLowerCase())
+        if(value.clientEmail.toString().trim().toLowerCase() === email.toString().trim().toLowerCase()) {
+            console.log("Here is the key: ", key);
+            return key;
+        }
+    }
+    return undefined;
 }
 
 broadCastMessage = message => {
@@ -39,7 +58,7 @@ broadCastMessage = message => {
 server.on('connection',ws => {
 
     /**
-     * Scenario1: Whenever we see that a new client ahs joined then do foloowing steps
+     * Scenario1: Whenever we see that a new client ahs joined then do following steps
      * 1. add that client to the client map
      * 2. greet the client with their new id
      * 3. tell others about a new client has joined with the new message of clientID
@@ -69,10 +88,31 @@ server.on('connection',ws => {
                 let clientName = data.clientName;
                 let clientID = data.clientID;
                 let profilePic = data.profilePic;
-                console.log(clientName, "has joined the collaborate");
-                clientInfo.set(clientID, { name: clientName, profilePic });
-                broadCastMessage(JSON.stringify(functionalMap.onClientJoined(clientID, clientName, profilePic)));
+                let clientLocation = data.location;
+                let clientEmail = data.clientEmail;
+
+                // check weather this email already existed or not
+                let prevClientEmailExistedID = checkEmailExistedThenReturnId(clientEmail);
+                console.log("Previous Existed ID: ", prevClientEmailExistedID);
+                if(prevClientEmailExistedID) {
+                    // re-map the previous client with this clientID
+                    clientMap.delete(clientID);
+                    clientMap.set(prevClientEmailExistedID, ws);
+                    clientID = prevClientEmailExistedID;
+                    clientInfo.set(clientID, {name: clientName, profilePic, clientLocation, clientEmail});
+                } else {
+                    console.log(clientName, "has joined the collaborate");
+                    clientInfo.set(clientID, {name: clientName, profilePic, clientLocation, clientEmail});
+                }
+
+                ws.send(JSON.stringify(functionalMap.acknowledgeClientInfo(clientID)));
+
+                let clientObj = functionalMap.onClientJoined(clientID, clientName, profilePic, clientLocation, clientEmail);
+                let stringifiedJSON = JSON.stringify(clientObj);
+                broadCastMessage(stringifiedJSON);
+
             } else if(data.responseEvent === 'CHAT') {
+                console.log("Chat Data: ", data);
                 broadCastMessage(JSON.stringify(data));
             }
         } else {
@@ -98,8 +138,7 @@ server.on('connection',ws => {
         clientMap.forEach((client, clientID) => {
             if(client === ws) {
                 clientMap.delete(clientID);
-                console.log(clientInfo.get(clientID).name, "has disconnected the collaborate");
-                clientInfo.delete(clientID);
+                console.log(clientInfo.get(clientID)?.name, "has disconnected the collaborate");
                 broadCastMessage(JSON.stringify(functionalMap.onClientDisconnected(clientID)));
             }
         });
